a11cfece8bc2e1a27ace0f9970f05a8067ad0323
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2024-10-08 21:28:14 +0400

    :bug: Preprocesses namespaced tags

--- src-tauri/src/podcast/mod.rs
+++ src-tauri/src/podcast/mod.rs
@@ -1,3 +1,4 @@
+use quick_xml::de::from_str;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize, Default, Clone)]
@@ -24,33 +25,34 @@ pub struct Feed {
 pub struct Podcast {
     #[serde(rename = "$text")]
     pub text: Option<String>,
-/*     #[serde(rename = "link")]
-    pub atom_link: AtomLink, */
+    #[serde(rename = "atom-link")]
+    pub atom_link: AtomLink,
     pub title: String,
-/*     pub link: String, */
+    pub link: String,
     pub language: String,
     pub copyright: String,
     pub description: String,
-/*     pub image: Image, */
-    #[serde(rename = "explicit")]
+    pub image: Image,
+    #[serde(rename = "itunes-explicit")]
     pub itunes_explicit: String,
-    #[serde(rename = "type")]
+    #[serde(rename = "itunes-type")]
     pub itunes_type: String,
-    #[serde(rename = "subtitle")]
+    #[serde(rename = "itunes-subtitle")]
     pub itunes_subtitle: String,
-    #[serde(rename = "author")]
+    #[serde(rename = "itunes-author")]
     pub itunes_author: String,
-    #[serde(rename = "summary")]
+    #[serde(rename = "itunes-summary")]
     pub itunes_summary: String,
     #[serde(rename = "encoded")]
     pub content_encoded: String,
-    #[serde(rename = "owner")]
+    #[serde(rename = "itunes-owner")]
     pub itunes_owner: ItunesOwner,
-/*     #[serde(rename = "image")]
+    /*     #[serde(rename = "image")]
     pub itunes_image: ChannelItunesImage, */
-    #[serde(rename = "category")]
+    #[serde(rename = "itunes-category")]
     pub itunes_category: ItunesCategory,
-    pub item: Vec<Item>,
+    #[serde(rename = "item")]
+    pub episodes: Vec<Episode>,
 }
 
 #[derive(Debug, Serialize, Deserialize, Default, Clone)]
@@ -76,9 +78,9 @@ pub struct Image {
 pub struct ItunesOwner {
     #[serde(rename = "$text")]
     pub text: Option<String>,
-    #[serde(rename = "name")]
+    #[serde(rename = "itunes-name")]
     pub itunes_name: String,
-    #[serde(rename = "email")]
+    #[serde(rename = "itunes-email")]
     pub itunes_email: String,
 }
 
@@ -95,36 +97,36 @@ pub struct ItunesCategory {
 }
 
 #[derive(Debug, Serialize, Deserialize, Default, Clone)]
-pub struct Item {
+pub struct Episode {
     #[serde(rename = "$text")]
     pub text: Option<String>,
-    #[serde(rename = "image")]
+    #[serde(rename = "itunes-image")]
     pub itunes_image: Option<ItemItunesImage>,
-    #[serde(rename = "episode")]
+    #[serde(rename = "itunes-episode")]
     pub itunes_episode: Option<String>,
-    #[serde(rename = "season")]
+    #[serde(rename = "itunes-season")]
     pub itunes_season: Option<String>,
-/*     pub title: String, */
+    pub title: String,
     pub description: String,
     #[serde(rename = "pubDate")]
     pub pub_date: String,
-/*     #[serde(rename = "title")]
-    pub itunes_title: String, */
-    #[serde(rename = "episodeType")]
+    #[serde(rename = "itunes-title")]
+    pub itunes_title: String,
+    #[serde(rename = "itunes-episodeType")]
     pub itunes_episode_type: String,
-    #[serde(rename = "author")]
+    #[serde(rename = "itunes-author")]
     pub itunes_author: String,
-    #[serde(rename = "subtitle")]
+    #[serde(rename = "itunes-subtitle")]
     pub itunes_subtitle: String,
-    #[serde(rename = "summary")]
+    #[serde(rename = "itunes-summary")]
     pub itunes_summary: String,
     #[serde(rename = "encoded")]
     pub content_encoded: String,
-    #[serde(rename = "duration")]
+    #[serde(rename = "itunes-duration")]
     pub itunes_duration: String,
     pub guid: Guid,
     pub enclosure: Enclosure,
-    #[serde(rename = "explicit")]
+    #[serde(rename = "itunes-explicit")]
     pub itunes_explicit: Option<String>,
     pub link: Option<String>,
 }
@@ -153,11 +155,21 @@ pub struct Enclosure {
     pub enclosure_type: String,
 }
 
+pub fn parse_feed(xml: &str) -> Feed {
+    from_str(&xml.replace("itunes:", "itunes-").replace("atom:", "atom-")).unwrap()
+}
+
 #[test]
 fn parses_xml() {
-  use quick_xml::de::from_str;
     let xml = include_str!("../../test_data/feed.rss");
-    let feed: Feed = from_str(xml).unwrap();
-    assert_eq!(feed.podcast.title, "Regulation Podcast");
-    assert_eq!(feed.podcast.description, "The Regulation Podcast is a show about friendship and embracing absurdity. Join Andrew, Nick, Eric, Geoff and Gavin in a never-ending pursuit to make each other laugh at almost any cost. Whether it's Podcasts, Let's Plays or Anal Waxings something incredibly stupid is always happening and we'd love for you to be along for the ride.\u{a0}");
+    let podcast = parse_feed(xml).podcast;
+    assert_eq!(podcast.atom_link.href, "https://feeds.megaphone.fm/fface");
+    assert_eq!(podcast.title, "Regulation Podcast");
+    assert_eq!(podcast.description, "The Regulation Podcast is a show about friendship and embracing absurdity. Join Andrew, Nick, Eric, Geoff and Gavin in a never-ending pursuit to make each other laugh at almost any cost. Whether it's Podcasts, Let's Plays or Anal Waxings something incredibly stupid is always happening and we'd love for you to be along for the ride.\u{a0}");
+    assert_eq!(podcast.image.url, "https://megaphone.imgix.net/podcasts/8f49d8ba-9de6-11ea-8184-d33047a704dc/image/b0198dcae0e6a8eddec040c27f20cded.jpg?ixlib=rails-4.3.1 &max-w=3000 &max-h=3000 &fit=crop &auto=format,compress");
+
+    assert!(podcast
+        .episodes
+        .iter()
+        .any(|episode| episode.title == "Why We Sync // Fastball vs Linebacker [21]"))
 }
