536ec99acd86268b80dfaba399a0c17d32a540c1
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2024-10-13 09:38:32 +0400

    :sparkles: Adds window State tracking

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -6,6 +6,7 @@ use tauri_plugin_fs::FsExt;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_window_state::Builder::new().build())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![
