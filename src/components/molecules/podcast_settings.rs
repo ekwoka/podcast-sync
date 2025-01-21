3d0bae043d0b93f701201f2be68da140f5dcd3ab
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2025-01-21 20:59:47 +0400

    :construction_worker: Adds Dev Container
    Cannot build due to upstream bug impacting wasm-opt

--- src/components/molecules/podcast_settings.rs
+++ src/components/molecules/podcast_settings.rs
@@ -7,7 +7,7 @@ stylance::import_crate_style!(styles, "src/components/molecules/podcast_settings
 pub fn podcast_settings(podcast_id: usize) -> impl IntoView {
     view! {
         <div class=styles::list_box>
-            <Button btn_type=ButtonType::Button>unsubscribe</Button>
+            <Button btn_type=ButtonType::Button>unsubscribe from {format!("{podcast_id}")}</Button>
         </div>
     }
 }
